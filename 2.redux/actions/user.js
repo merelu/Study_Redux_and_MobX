ecc08146bd71973d95a7050cd0c469e80800298e
GYUHA_HWANG <gyuha0310@gmail.com>
2021-05-06 20:20:00 +0900

    thunk 미들웨어 구조 and  미들웨어 작동방식

--- 2.redux/actions/user.js
+++ 2.redux/actions/user.js
@@ -1,7 +1,40 @@
 const logIn = (data) => {
+  //async action creator - function 리턴
+  return (dispatch, getstate) => {
+    dispatch(logInRequest(data));
+    try {
+      setTimeout(() => {
+        dispatch(
+          logInSuccess({
+            userId: 1,
+            nickname: "gyuha",
+          })
+        );
+      }, 2000);
+    } catch (e) {
+      dispatch(logInFailure(e));
+    }
+  };
+};
+
+const logInRequest = (data) => {
   return {
-    type: "LOG_IN",
+    type: "LOG_IN_REQUEST",
     data,
+  };
+};
+
+const logInSuccess = (data) => {
+  return {
+    type: "LOG_IN_SUCCESS",
+    data,
+  };
+};
+
+const logInFailure = (error) => {
+  return {
+    type: "LOG_IN_FAILURE",
+    error,
   };
 };
 
--- 2.redux/index.js
+++ 2.redux/index.js
@@ -11,10 +11,31 @@ const initialState = {
   posts: [],
 };
 
-const store = createStore(reducer, initialState);
+//action과 reducer, dispatch는 동기적이므로 비동기 처리가 불가능하다. 미들웨어를 통해 비동기 처리를 한다.
+const fistMiddleware = (store) => (dispatch) => (action) => {
+  console.log("액션 로깅", action);
+  dispatch(action); //이것만 있음 기본기능
+};
+// function firstMiddleware(store) {
+//   return function (next) {
+//     return function (action) {};
+//   };
+// }
+
+const thunkMiddleware = (store) => (next) => (action) => {
+  if (typeof action === "function") {
+    //비동기 - 비동기인경우 action을 함수로 넣어주겠다. action은 객체
+    return action(store.dispatch, store.getState);
+  }
+  next(action); //동기
+};
+
+const enhancer = applyMiddleware(fistMiddleware, thunkMiddleware);
+
+const store = createStore(reducer, initialState, enhancer);
 
 store.subscribe(() => {
-  console.log("changed");
+  console.log("subscribe", "changed");
 });
 console.log("1st", store.getState());
 //action 객체를 action이라고 한다.
@@ -27,25 +48,25 @@ store.dispatch(
   })
 );
 
-console.log("2nd", store.getState());
+// console.log("2nd", store.getState());
 
-store.dispatch(
-  addPost({
-    userId: 1,
-    id: 1,
-    content: "안녕하세요.",
-  })
-);
+// store.dispatch(
+//   addPost({
+//     userId: 1,
+//     id: 1,
+//     content: "안녕하세요.",
+//   })
+// );
 
-store.dispatch(
-  addPost({
-    userId: 1,
-    id: 2,
-    content: "두번째 포스트.",
-  })
-);
+// store.dispatch(
+//   addPost({
+//     userId: 1,
+//     id: 2,
+//     content: "두번째 포스트.",
+//   })
+// );
 
-console.log("3rd", store.getState());
+// console.log("3rd", store.getState());
 
-store.dispatch(logOut());
-console.log("4th", store.getState());
+// store.dispatch(logOut());
+// console.log("4th", store.getState());
