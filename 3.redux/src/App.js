ea1d3fbaecf91cf7ba84f881ec2c9b76b9dfed69
GYUHA_HWANG <gyuha0310@gmail.com>
2021-05-08 20:10:31 +0900

    redux를 이용한 간단한 프로그램

--- 3.redux/src/App.js
+++ 3.redux/src/App.js
@@ -1,5 +1,37 @@
+import React, { useCallback } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { logIn, logOut } from "./actions/user";
+
 function App() {
-  return <div className="App">안영</div>;
+  const user = useSelector((state) => state.user);
+  const dispatch = useDispatch();
+  const onLogIn = useCallback(() => {
+    dispatch(
+      logIn({
+        id: "gyuha",
+        password: "1234",
+      })
+    );
+  }, [dispatch]);
+  const onLogout = useCallback(() => {
+    dispatch(logOut());
+  }, []);
+  return (
+    <div>
+      {user.isLoggingIn ? (
+        <div>로그인중</div>
+      ) : user.data ? (
+        <div>{user.data.nickname}</div>
+      ) : (
+        <div>로그인 해주세요.</div>
+      )}
+      {!user.data ? (
+        <button onClick={onLogIn}>로그인</button>
+      ) : (
+        <button onClick={onLogout}>로그아웃</button>
+      )}
+    </div>
+  );
 }
 
 export default App;
