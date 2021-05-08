9f32bfcc5402cf23ed9bbb094e30365faf91732d
GYUHA_HWANG <gyuha0310@gmail.com>
2021-05-08 21:07:29 +0900

    .

--- 4.react-redux-immer/src/App.js
+++ 4.react-redux-immer/src/App.js
@@ -1,10 +1,10 @@
-import React, { useCallback } from "react";
+import React from "react";
 import { connect } from "react-redux";
 import { logIn, logOut } from "./actions/user";
 
 class App {
   onLogIn = () => {
-    dispatch(
+    this.props.dispatchLogIn(
       logIn({
         id: "gyuha",
         password: "1234",
@@ -12,7 +12,7 @@ class App {
     );
   };
   onLogOut = () => {
-    dispatch(logOut());
+    this.props.dispatchLogOut(logOut());
   };
   render() {
     const { user } = this.props;
@@ -26,9 +26,9 @@ class App {
           <div>로그인 해주세요.</div>
         )}
         {!user.data ? (
-          <button onClick={onLogIn}>로그인</button>
+          <button onClick={this.onLogIn}>로그인</button>
         ) : (
-          <button onClick={onLogOut}>로그아웃</button>
+          <button onClick={this.onLogOut}>로그아웃</button>
         )}
       </div>
     );
@@ -39,7 +39,9 @@ const mapStateToProps = (state) => ({
   user: state.user,
 }); //reselect
 const mapDispatchToProps = (dispatch) => ({
-  logIn: () => dispatch(logIn()),
-  logOut: () => dispatch(logOut()),
+  dispatchLogIn: () => dispatch(logIn()),
+  dispatchLogOut: () => dispatch(logOut()),
 });
 export default connect(mapStateToProps, mapDispatchToProps)(App);
+
+//connect : High Order Component(HOC)
