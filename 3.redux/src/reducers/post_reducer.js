1d390520a3124d1cd75dcde24483d7a5e4388817
GYUHA_HWANG <gyuha0310@gmail.com>
2021-05-08 20:11:39 +0900

    reducer 작성

---
+++ 3.redux/src/reducers/post_reducer.js
@@ -0,0 +1,14 @@
+import { ADD_POST } from "../actions/type";
+
+const initialState = [];
+
+const postReducer = (prevState = initialState, action) => {
+  switch (action.type) {
+    case ADD_POST:
+      return [...prevState, action.data];
+    default:
+      return prevState;
+  }
+};
+
+export default postReducer;
